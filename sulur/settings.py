dabd78139c6f10bd535cfa59403d287845e914ab
rizalhamdana <ari.gusti12@gmail.com>
2019-03-17 15:41:38 +0800

    host heroku

--- sulur/settings.py
+++ sulur/settings.py
@@ -24,7 +24,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
