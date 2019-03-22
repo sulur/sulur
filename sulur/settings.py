ecdf82623b7fed6d10b94214ed6504782ba14fdd
rizalhamdana <ari.gusti12@gmail.com>
2019-03-23 00:56:10 +0800

    host

--- sulur/settings.py
+++ sulur/settings.py
@@ -26,7 +26,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG =
-DEBUG = True
+DEBUG = config('DEBUG', defualt=true, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
