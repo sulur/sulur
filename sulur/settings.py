12942ac7617dd98881f0cb2bef8ca01296000d83
rizalhamdana <ari.gusti12@gmail.com>
2019-03-23 01:31:45 +0800

    host

--- sulur/settings.py
+++ sulur/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', 'sulur.herokuapp.com',
-                 '127.0.0.1', '192.168.31.144', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
